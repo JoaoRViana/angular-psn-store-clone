fecc09a112c2aee03b9b964b809bd87e9b5caea9
JoaoRViana <joaorviana98@gmail.com>
2024-11-09 13:40:08 -0300

    feat: adjust make more dynamacilly render contents

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { fakeData } from 'src/app/data';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  contents:any
+
+  constructor() { 
+    this.contents = fakeData
+  }
 
   ngOnInit(): void {
   }
